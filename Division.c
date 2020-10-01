5ea4582b2debb4c1e37e0033f5da70a4fcc9a20c
ankitsharma739501 <68945056+ankitsharma739501@users.noreply.github.com>
2020-10-01 17:36:18 +0530

    Update Division.c

--- Division.c
+++ Division.c
@@ -1,8 +1,12 @@
+//division of two numbers
+
+
 #include<stdio.h>
 void main()
-{ int a,b,c;
-printf("Enter two Numbers : ");
-scanf("%d %d",&a,&b);
-c=a/b;
-printf("Division is %d",c);
+{ 
+  int a,b,c;
+  printf("Enter two Numbers : \n");
+  scanf("%d %d",&a,&b);
+  c=a/b;
+  printf("Division of a by b is %d",c);
 }
