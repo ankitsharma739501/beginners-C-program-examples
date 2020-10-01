ed31fc5125563aac65a4b0f86f669360a0f15e63
ankitsharma739501 <68945056+ankitsharma739501@users.noreply.github.com>
2020-10-01 17:53:58 +0530

    Update oddandeven.c

--- oddandeven.c
+++ oddandeven.c
@@ -1,16 +1,15 @@
 // Program to tell if an integer number is even or odd
 
 #include <stdio.h>
-
 int main(){
 
-int number;
-printf("Enter an integer number\n");
-scanf("%d",&number);
-if (number %2 ==0)
+  int number;
+   printf("Enter an integer number\n");
+   scanf("%d",&number);
+   if (number %2 ==0)
    printf("The number %d is even\n",number);
-else{
+   else{
    printf("The number %d is odd\n",number);
-}
+       }
 return 0;
 }
